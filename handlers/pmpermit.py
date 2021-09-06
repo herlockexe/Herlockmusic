9ebd8e1198cf8b23ef5fce0cfc92f06114887bea
EkimozSupport <88406196+EkimozSupport@users.noreply.github.com>
2021-09-06 05:39:09 +0300

    Update and rename handlers/pmpermit.py to işleyiciler/pmpermit.py

--- handlers/pmpermit.py
+++
@@ -1,9 +0,0 @@
-from callsmusic.callsmusic import client as USER
-from pyrogram import filters
-from pyrogram.types import Chat, Message, User
-
-
-@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
-async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Merhaba 😎,, Nasılsın şimdi şöyleki bu Userbir bot sohbeti.\n\n⚠️Rules:\n- Sohbet etmek yasak bilgileri Okuyunuz\n-Mesaj yazma spam sayılıyor. \n\n 🚨 **MÜZİK BOTUNU GRUPLARINIZA ALMAK İÇİN /katil KOMUTUNA BASINIZ İLK YAPMANIZ GEREKEN, BOTU GRUBUNUZA EKLEMEK** @TurkishVoicebot * Denemeye değer 🤔**\n\n**Bilgileri okudunuz. KİŞİSEL BİLGİLERİ Paylaşmayınız. İyi günler diliyorum. 🚨**\n\n**🤖 Developed by @EfsaneStar**")
-  return                        
