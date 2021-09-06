acef07effcba1585939b8dfae4aec2b114cfd313
EkimozSupport <88406196+EkimozSupport@users.noreply.github.com>
2021-09-06 05:22:23 +0300

    Update and rename handlers/userbotjoin.py to işleyiciler/userbotjoin.py

--- işleyiciler/userbotjoin.py
+++ işleyiciler/userbotjoin.py
@@ -20,7 +20,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "Rgyardimci"
+        user.first_name =  "Hatirayardimci"
 
     try:
         await USER.join_chat(invitelink)
